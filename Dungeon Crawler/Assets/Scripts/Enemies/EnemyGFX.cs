4b1e0cd8763c5c31e0745c8b9a1707c0457d565f
Mikael Remes <pojankolli@gmail.com>
2020-01-26 14:42:16 +0200

    Animated player

---
+++ Dungeon Crawler/Assets/Scripts/Enemies/EnemyGFX.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aiPath;
+    public float movementTreshHold = 0.01f;
+    
+    public Sprite idle;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //idle
+        if(aiPath.desiredVelocity.x <= movementTreshHold && aiPath.desiredVelocity.x > -movementTreshHold && aiPath.desiredVelocity.y <= movementTreshHold && aiPath.desiredVelocity.y > -movementTreshHold)
+        {
+            //TODO: use idle animation
+        }
+        //moving right
+        else if(aiPath.desiredVelocity.x > movementTreshHold)
+        {
+            //TODO: use right animation
+        }
+        //moving left
+        else if(aiPath.desiredVelocity.x < -movementTreshHold)
+        {
+            //TODO: use left animation
+        }
+        //moving down
+        else if (aiPath.desiredVelocity.y < -movementTreshHold)
+        {
+            //TODO: use down animation
+        }
+        //moving up
+        else if (aiPath.desiredVelocity.y > movementTreshHold)
+        {
+            //TODO: use up animation
+        }
+
+    }
+}
--- Dungeon Crawler/Assets/Scripts/Player/PlayerMovement.cs
+++ Dungeon Crawler/Assets/Scripts/Player/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     public GameObject runEffect;
     public float runEffectCoolDown = 0.2f;
     public float runEffectDistanceFromPlayer = 0.2f;
+    public Animator animator;
 
     private Playerstats stats;
     private float runEffectCoolDownTimer = 0.2f;
@@ -26,6 +27,8 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
 
+        animator.SetFloat("SpeedY", 0f);
+        animator.SetFloat("SpeedX", 0f);
         //movements
         if (Input.GetKey("w"))
         {
@@ -33,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
             //playerVelocity = new Vector2(playerVelocity.x, maxMovementSpeed);
             if (playerVelocity.y < maxMovementSpeed) GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, movementForce * Time.deltaTime));
             DoRunEffect(new Vector2(transform.position.x, transform.position.y - runEffectDistanceFromPlayer));
+            animator.SetFloat("SpeedY", 1f);
         }
         if (Input.GetKey("a"))
         {
@@ -40,6 +44,7 @@ public class PlayerMovement : MonoBehaviour
             //playerVelocity = new Vector2(-maxMovementSpeed, playerVelocity.y);
             if (playerVelocity.x > -maxMovementSpeed) GetComponent<Rigidbody2D>().AddForce(new Vector2(-movementForce * Time.deltaTime, 0f));
             DoRunEffect(new Vector2(transform.position.x + runEffectDistanceFromPlayer, transform.position.y));
+            animator.SetFloat("SpeedX", -1f);
         }
         if (Input.GetKey("s"))
         {
@@ -47,6 +52,7 @@ public class PlayerMovement : MonoBehaviour
             //playerVelocity = new Vector2(playerVelocity.x, -maxMovementSpeed);
             if (playerVelocity.y > -maxMovementSpeed) GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, -movementForce * Time.deltaTime));
             DoRunEffect(new Vector2(transform.position.x, transform.position.y + runEffectDistanceFromPlayer));
+            animator.SetFloat("SpeedY", -1f);
         }
         if (Input.GetKey("d"))
         {
@@ -54,6 +60,7 @@ public class PlayerMovement : MonoBehaviour
             //playerVelocity = new Vector2(maxMovementSpeed, playerVelocity.y);
             if (playerVelocity.x < maxMovementSpeed) GetComponent<Rigidbody2D>().AddForce(new Vector2(movementForce * Time.deltaTime, 0f));
             DoRunEffect(new Vector2(transform.position.x - runEffectDistanceFromPlayer, transform.position.y));
+            animator.SetFloat("SpeedX", 1f);
         }
 
         if (runEffectCoolDownTimer >= 0f) runEffectCoolDownTimer -= Time.deltaTime;
