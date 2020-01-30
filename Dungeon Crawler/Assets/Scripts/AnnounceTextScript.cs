c2d3ae9cddb30de5cd7a6a530a975f3b934b92dc
Mikael Remes <pojankolli@gmail.com>
2020-01-30 11:17:55 +0200

    small changes

---
+++ Dungeon Crawler/Assets/Scripts/AnnounceTextScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnnounceTextScript : MonoBehaviour
+{
+    public float delay = 5f;
+    public float fadespeed = 0.2f;
+
+    private float delaytimer = 5f;
+
+    // start fading text after delay
+    void Update()
+    {
+        delaytimer -= Time.deltaTime;
+        if (delaytimer <= 0)
+        {
+            if (GetComponent<Text>().color.a > 0)
+            {
+                GetComponent<Text>().color = new Color(1, 0f, 0f, GetComponent<Text>().color.a - Time.deltaTime*fadespeed);
+            }
+        }
+    }
+
+    public void announce(string text)
+    {
+        delaytimer = delay;
+    }
+}
--- Dungeon Crawler/Assets/Scripts/Dungeon Scripts/DungeonRoomGenerator.cs
+++ Dungeon Crawler/Assets/Scripts/Dungeon Scripts/DungeonRoomGenerator.cs
@@ -18,21 +18,27 @@ public class DungeonRoomGenerator : MonoBehaviour
     {
         level++;
         GameObject room;
-        if(level != firstBossRoom)room = Instantiate(BasicRoomTemplate, new Vector2(0, roomOffSetPerLevel*level), Quaternion.identity);
-        else room = Instantiate(BossRoomTemplate, new Vector2(0, roomOffSetPerLevel * level), Quaternion.identity);
-
+        //end game if final level
         if (level == finalLevel + 1) gameOver.GetComponent<GameoverScript>().GameOver(false);
+        //else generate new room
+        else
+        {
+            if (level != firstBossRoom) room = Instantiate(BasicRoomTemplate, new Vector2(0, roomOffSetPerLevel * level), Quaternion.identity);
+            else room = Instantiate(BossRoomTemplate, new Vector2(0, roomOffSetPerLevel * level), Quaternion.identity);
 
-        if (room.GetComponent<RoomGenerator>() != null) room.GetComponent<RoomGenerator>().GenerateObjectsAndEnemies(level);
 
-        GridGraph gg = pathFindingMesh.GetComponent<AstarPath>().data.gridGraph;
-        Debug.Log("0" + gg.width);
-        int width = gg.width;
-        int depth = gg.depth + 60;
-        float nodeSize = gg.nodeSize;
-        gg.SetDimensions(width, depth, nodeSize);
-        //TODO move graph to map center for pathfinding efficiency
-        Invoke("scanGraphs", 0.1f);
+
+            if (room.GetComponent<RoomGenerator>() != null) room.GetComponent<RoomGenerator>().GenerateObjectsAndEnemies(level);
+
+            GridGraph gg = pathFindingMesh.GetComponent<AstarPath>().data.gridGraph;
+            Debug.Log("0" + gg.width);
+            int width = gg.width;
+            int depth = gg.depth + 60;
+            float nodeSize = gg.nodeSize;
+            gg.SetDimensions(width, depth, nodeSize);
+            //TODO move graph to map center for pathfinding efficiency
+            Invoke("scanGraphs", 0.1f);
+        }
     }
 
     public void scanGraphs()
