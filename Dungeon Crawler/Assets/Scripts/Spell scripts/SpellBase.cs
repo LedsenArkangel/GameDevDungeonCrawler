649de976c8b6834f3f9a60c8d174f6e491af9b52
Mikael Remes <pojankolli@gmail.com>
2020-02-02 18:34:07 +0200

    created barrage spell

--- Dungeon Crawler/Assets/Scripts/Spell scripts/SpellBase.cs
+++ Dungeon Crawler/Assets/Scripts/Spell scripts/SpellBase.cs
@@ -30,13 +30,9 @@ public class SpellBase : MonoBehaviour
 
             switch (spell)
             {
-                case Spell.NINJASTAR:
-                    GameObject ninjastar = Instantiate(projectile, player.transform.position, Quaternion.identity);
-                    ninjastar.GetComponent<ProjectileScript>().initializeProjectile(getDirectionFromMouseToPlayer(player), player.GetComponent<Collider2D>());
-                    break;
-                case Spell.FIREPOTION:
-                    GameObject firepotion = Instantiate(projectile, player.transform.position, Quaternion.identity);
-                    firepotion.GetComponent<ProjectileScript>().initializeProjectile(getDirectionFromMouseToPlayer(player), player.GetComponent<Collider2D>());
+                case Spell.PROJECTILE:
+                    GameObject projectileObject = Instantiate(projectile, player.transform.position, Quaternion.identity);
+                    projectileObject.GetComponent<ProjectileScript>().initializeProjectile(getDirectionFromMouseToPlayer(player), player.GetComponent<Collider2D>());
                     break;
                 case Spell.HEAL:
                     player.GetComponent<Playerstats>().heal(10);
@@ -45,21 +41,17 @@ public class SpellBase : MonoBehaviour
                 case Spell.SMITE:
                     if (GetComponent<Smite>() != null) GetComponent<Smite>().useSmite(player, Camera.main.ScreenToWorldPoint(Input.mousePosition), spellUseEffectPrimary, spellUseEffectSecondary);
                     break;
-                case Spell.POISONBOLT:
-                    GameObject poisonBolt = Instantiate(projectile, player.transform.position, Quaternion.identity);
-                    poisonBolt.GetComponent<ProjectileScript>().initializeProjectile(getDirectionFromMouseToPlayer(player), player.GetComponent<Collider2D>());
-                    break;
-                case Spell.FROSTBOLT:
-                    GameObject frostBolt = Instantiate(projectile, player.transform.position, Quaternion.identity);
-                    frostBolt.GetComponent<ProjectileScript>().initializeProjectile(getDirectionFromMouseToPlayer(player), player.GetComponent<Collider2D>());
-                    break;
-                case Spell.FIREBOLT:
-                    GameObject fireBolt = Instantiate(projectile, player.transform.position, Quaternion.identity);
-                    fireBolt.GetComponent<ProjectileScript>().initializeProjectile(getDirectionFromMouseToPlayer(player), player.GetComponent<Collider2D>());
-                    break;
-                case Spell.OILPOTION:
-                    GameObject oilpotion = Instantiate(projectile, player.transform.position, Quaternion.identity);
-                    oilpotion.GetComponent<ProjectileScript>().initializeProjectile(getDirectionFromMouseToPlayer(player), player.GetComponent<Collider2D>());
+                case Spell.BARRAGE:
+                    GameObject projectileObject1 = Instantiate(projectile, player.transform.position, Quaternion.identity);
+                    projectileObject1.GetComponent<ProjectileScript>().initializeProjectile(getDirectionFromMouseToPlayer(player), player.GetComponent<Collider2D>());
+                    GameObject projectileObject2 = Instantiate(projectile, player.transform.position, Quaternion.identity);
+                    projectileObject2.GetComponent<ProjectileScript>().initializeProjectile(RotateVector(getDirectionFromMouseToPlayer(player), 45), player.GetComponent<Collider2D>());
+                    GameObject projectileObject3 = Instantiate(projectile, player.transform.position, Quaternion.identity);
+                    projectileObject3.GetComponent<ProjectileScript>().initializeProjectile(RotateVector(getDirectionFromMouseToPlayer(player), -45), player.GetComponent<Collider2D>());
+                    GameObject projectileObject4 = Instantiate(projectile, player.transform.position, Quaternion.identity);
+                    projectileObject4.GetComponent<ProjectileScript>().initializeProjectile(RotateVector(getDirectionFromMouseToPlayer(player), 22), player.GetComponent<Collider2D>());
+                    GameObject projectileObject5 = Instantiate(projectile, player.transform.position, Quaternion.identity);
+                    projectileObject5.GetComponent<ProjectileScript>().initializeProjectile(RotateVector(getDirectionFromMouseToPlayer(player), -22), player.GetComponent<Collider2D>());
                     break;
                 default:
                     Debug.Log("spell not defined");
@@ -77,9 +69,21 @@ public class SpellBase : MonoBehaviour
         return new Vector2(towardsMouseFromPlayer.x, towardsMouseFromPlayer.y);
     }
 
+    public Vector2 RotateVector(Vector2 v, float degrees)
+    {
+        float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+        float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+        float tx = v.x;
+        float ty = v.y;
+        v.x = (cos * tx) - (sin * ty);
+        v.y = (sin * tx) + (cos * ty);
+        return v;
+    }
+
     public enum Spell
     {
-        UNDEFINED, NINJASTAR, FIREPOTION, HEAL, SMITE, POISONBOLT, FROSTBOLT, FIREBOLT, OILPOTION
+        UNDEFINED, PROJECTILE, SMITE, HEAL, BARRAGE
     }
 
 }
